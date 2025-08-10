2aee2c16e923c673fa8e5753ed69c437ceb9e900
x0cloud69 <x0cloud69@gmail.com>
2025-08-10 15:55:07 +0900

    Change app.py

--- ky-codyssey-main/Codyseey/app.py
+++ ky-codyssey-main/Codyseey/app.py
@@ -28,5 +28,10 @@ def home():
     hostname = ''
   return render_template('index.html',debug=True,computername=hostname)
 
+@app.route('/test1')
+def test1():
+  return render_template('test1.html')
+
+
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=80, debug=True)
