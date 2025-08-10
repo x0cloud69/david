54a700c15a7d1665261ce03f9685e53c49d82540
x0cloud69 <x0cloud69@gmail.com>
2025-08-10 14:20:47 +0900

    1-3. Computer Name

--- ky-codyssey-main/Codyseey/app.py
+++ ky-codyssey-main/Codyseey/app.py
@@ -1,10 +1,13 @@
 # render_template : 템플릿 파일을 렌더링하여 HTML 페이지를 생성하는 함수
 from flask import Flask,render_template
+import socket
 
 #Flask 웹 애플리케이션을 초기화
 #__name__ : 현재 실행 중인 모듈의 이름을 담고 있습니다
 #직접 실행할 경우 "__main__"이 됩니다
 # 웹 애플리케이션 개발에 필요한 다양한 기능을 Flask 클래스에서 제공
+# Hunk 단위로 Git 커밋 가능
+# Font size 50 pt
 
 app = Flask(__name__)
 
@@ -17,5 +20,13 @@ def hello_world():
 def menu():
   return render_template('menu.html')
 
+@app.route('/index')
+def home():
+  if app.debug:
+    hostname = '컴퓨터(인스턴스) : ' + socket.gethostname()
+  else:
+    hostname = ''
+  return render_template('index.html',debug=True,computername=hostname)
+
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=80)
+   app.run(host='0.0.0.0', port=80, debug=True)
